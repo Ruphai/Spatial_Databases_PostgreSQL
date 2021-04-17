153d538559ab1eb6bdb56fe5b2db8b57413c7098
Rufai Balogun <59208190+Ruphai@users.noreply.github.com>
2021-04-17 14:29:48 +0200

    Additional Geometry Functions
    Some added geometries functions in POSTGIS. Most ideas are well explained in the POSTGIS documentation.

--- geometries_functions.sql
+++ geometries_functions.sql
@@ -30,7 +30,44 @@ SELECT
 	ST_GeometryType(geom),
 	ST_Length(geom::geography), 
 	ST_Length(ST_Transform(geom, 26913)),
+	ST_LengthSpheroid(geom, 'SPHEROID["GRS_1980",6378137,298.257222101]'),
 	((ST_Length(geom::geography) - ST_Length(ST_Transform(geom, 26913)))/ST_Length(geom::geography))*100 AS diff
 		FROM linear_projects
 		ORDER BY diff DESC;
+
+SELECT
+	habitat_id, 
+	ST_Area(geom::geography), 
+	ST_Perimeter(geom::geography)
+		FROM buowl_habitat;
 		
+
+-- IV. CALCULATE DISTANCES FROM OTHER GEOMETRIES.
+
+-- V. SEE ITS COORDINATES IN TEXT FORM
+SELECT
+	nest_id, 
+	ST_AsText(geom), 
+	ST_AsEWKT(geom), 
+	ST_AsGeoJSON(ST_Transform(geom, 26913), 1), 
+	ST_AsGML(geom), 
+	ST_AsKML(geom) 
+		FROM baea_nests;
+
+-- VI. ACCESS INDIVIDUAL COORDINATES
+SELECT	
+	nest_id,
+	long_x_dd,
+	lat_y_dd,
+	ST_X(geom), 
+	ST_Y(geom), 
+	ST_X(ST_Transform(geom, 26913)), -- Transform to UTM
+	ST_Y(ST_Transform(geom, 26913))
+		FROM baea_nests;
+
+SELECT
+	project, 
+	ST_AsText(ST_startpoint(ST_GeometryN(geom, 1))) AS startpoint,
+	ST_AsText(ST_LineInterpolatePoint(ST_GeometryN(geom, 1), 0.5)) AS midpoint, --MidPoint
+	ST_AsText(ST_endpoint(ST_GeometryN(geom, 1))) AS endpoint
+		FROM linear_projects;
